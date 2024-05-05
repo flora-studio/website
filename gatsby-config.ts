8ea7e7a9bd1388b038936ac6b97a1d6bf1373d40
maliut <lqn619@163.com>
2024-05-05 21:18:21 +0800

    add deps

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,21 +2,28 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `florastudio-official2`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `芙露拉工作室 | Flora Studio`,
+    siteUrl: `https://florastudio.app`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: [
+    /*"gatsby-plugin-google-gtag",*/
+    'gatsby-plugin-postcss',
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+    }
+  ]
 };
 
 export default config;
