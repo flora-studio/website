19600276ff5ed6b20a655398d47b52de83b9deea
maliut <lqn619@163.com>
2024-05-12 17:07:17 +0800

    old mobile compact

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,51 +1,56 @@
-import React from 'react'
-import { PlatformIcon } from './svg/platform'
+import React, { PropsWithChildren, useState } from 'react'
+import { Platform, PlatformIcon } from './svg/platform'
 import { StaticImage } from 'gatsby-plugin-image'
+import { Popover } from 'react-tiny-popover'
+
+function Link({ platform, href }: { platform: Platform, href: string }) {
+  const urlProps = href.startsWith('mailto') ? {} : { target: '_blank', rel: 'noreferrer' }
+  return (
+    <a role="button" className="btn btn-ghost btn-sm btn-circle" href={href} {...urlProps}>
+      <PlatformIcon platform={platform} className="text-lg"/>
+    </a>
+  )
+}
+
+function Pop({ platform, children }: PropsWithChildren<{ platform: Platform }>) {
+  const [open, setOpen] = useState(false)
+  return (
+    <Popover
+      isOpen={open}
+      positions="top"
+      padding={4}
+      content={
+        <div className="z-[1] shadow bg-base-100 rounded-box overflow-hidden">
+          {children}
+        </div>
+      }
+      onClickOutside={() => setOpen(false)}
+    >
+      <a role="button" className="btn btn-ghost btn-sm btn-circle" onClick={() => setOpen(v => !v)}>
+        <PlatformIcon platform={platform} className="text-lg"/>
+      </a>
+    </Popover>
+  )
+}
 
 function Footer() {
-  return <footer className="footer footer-center gap-y-2 p-10 text-base-content rounded relative">
+  return <footer className="footer footer-center gap-y-2 py-10 text-base-content rounded relative">
     <nav>
       <div className="grid grid-flow-col">
-        <a role="button" className="btn btn-ghost btn-sm btn-circle" href="mailto:maca@florastudio.app">
-          <PlatformIcon platform="email" className="text-lg"/>
-        </a>
-        <div className="dropdown dropdown-top">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-sm btn-circle">
-            <PlatformIcon platform="wechat" className="text-lg"/>
-          </div>
-          <div tabIndex={0} className="dropdown-content z-[1] p-1 shadow bg-base-100 rounded-box overflow-hidden">
-            <StaticImage src="../images/wx.jpg" alt="wechat qrcode" layout="fixed" width={129} height={129} />
-          </div>
-        </div>
-        <a role="button" className="btn btn-ghost btn-sm btn-circle" href="https://space.bilibili.com/688429881" target="_blank" rel="noreferrer">
-          <PlatformIcon platform="bilibili" className="text-lg"/>
-        </a>
-        <a role="button" className="btn btn-ghost btn-sm btn-circle" href="https://www.xiaohongshu.com/user/profile/5bd451818bcbda000162dc20" target="_blank" rel="noreferrer">
-          <PlatformIcon platform="xiaohongshu" className="text-lg"/>
-        </a>
-        <div className="dropdown dropdown-top">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-sm btn-circle">
-            <PlatformIcon platform="tiktok" className="text-lg"/>
-          </div>
-          <div tabIndex={0} className="dropdown-content z-[1] shadow bg-base-100 rounded-box overflow-hidden">
-            <StaticImage src="../images/tiktok.jpg" alt="tiktok qrcode" layout="fixed" width={224} height={272} />
-          </div>
-        </div>
-        <a role="button" className="btn btn-ghost btn-sm btn-circle" href="https://weibo.com/u/6714705016" target="_blank" rel="noreferrer">
-          <PlatformIcon platform="weibo" className="text-lg"/>
-        </a>
-        <a role="button" className="btn btn-ghost btn-sm btn-circle" href="https://www.taptap.cn/developer/252870" target="_blank" rel="noreferrer">
-          <PlatformIcon platform="taptap" className="text-lg"/>
-        </a>
-        <a role="button" className="btn btn-ghost btn-sm btn-circle" href="https://indienova.com/u/florastudio" target="_blank" rel="noreferrer">
-          <PlatformIcon platform="indienova" className="text-lg"/>
-        </a>
-        <a role="button" className="btn btn-ghost btn-sm btn-circle" href="https://florastudio.itch.io/" target="_blank" rel="noreferrer">
-          <PlatformIcon platform="itchio" className="text-lg"/>
-        </a>
-        <a role="button" className="btn btn-ghost btn-sm btn-circle" href="https://afdian.net/a/florastudio" target="_blank" rel="noreferrer">
-          <PlatformIcon platform="afadian" className="text-lg"/>
-        </a>
+        <Link platform="email" href="mailto:maca@florastudio.app" />
+        <Pop platform="wechat">
+          <StaticImage src="../images/wx.jpg" alt="wechat qrcode" layout="fixed" width={129} height={129} />
+        </Pop>
+        <Link platform="bilibili" href="https://space.bilibili.com/688429881" />
+        <Link platform="xiaohongshu" href="https://www.xiaohongshu.com/user/profile/5bd451818bcbda000162dc20" />
+        <Pop platform="tiktok">
+          <StaticImage src="../images/tiktok.jpg" alt="tiktok qrcode" layout="fixed" width={224} height={272} />
+        </Pop>
+        <Link platform="weibo" href="https://weibo.com/u/6714705016" />
+        <Link platform="taptap" href="https://www.taptap.cn/developer/252870" />
+        <Link platform="indienova" href="https://indienova.com/u/florastudio" />
+        <Link platform="itchio" href="https://florastudio.itch.io/" />
+        <Link platform="afadian" href="https://afdian.net/a/florastudio" />
       </div>
     </nav>
     <aside>
